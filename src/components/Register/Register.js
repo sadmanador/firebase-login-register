55c55b7298717d262c7cb48c1b9cce245900a337
sadmanador <107940313+GaziTasnuvaFarhana@users.noreply.github.com>
2022-12-04 00:32:26 +0600

    Create USer is using properly

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import { getAuth } from "firebase/auth";
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, parseActionCodeURL } from "firebase/auth";
 import { app } from '../../firebase/firebase.init';
 
 const auth = getAuth(app);
 
 const Register = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleCreateUser = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => console.error('error: ', error))
+    }
+
     return (
         <div>
-            <h1>This is register page</h1>
+            <form onSubmit={handleCreateUser}>
+                <label htmlFor="email">Email</label>
+                <input type="email" placeholder='Your email' name='email'/><br />
+                <label htmlFor="password">Password</label>
+                <input type="password" placeholder='Your password' name='password'/><br />
+                <button type='submit'>Register</button>
+            </form>
         </div>
     );
 };
