78f4de60ddc439fc8be3c1dc4f772ddc0a60ae8b
sadmanador <107940313+GaziTasnuvaFarhana@users.noreply.github.com>
2022-12-08 19:58:35 +0600

    password reset email is working

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from '../../firebase/firebase.init';
 import { useState } from 'react';
 
@@ -9,6 +9,10 @@ const auth = getAuth(app)
 const Login = () => {
     const [currentUser, setCurrentUser] = useState({});
     const [errorMessage, setErrorMessage] = useState('');
+    const [success, SetSuccess] = useState(false);
+    const [userEmail, SetUserEmail] = useState('');
+    const [resetEmail, setResetEmail] = useState(false);
+
 
     const handleLogin = event => {
         event.preventDefault();
@@ -17,32 +21,55 @@ const Login = () => {
         const password = form.password.value;
         console.log(email, password);
 
+        //signIn user
         signInWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            const user = result.user;
-            setCurrentUser(user);
+            .then(result => {
+                const user = result.user;
+                setCurrentUser(user);
+                SetSuccess(true);
+                form.reset();
+                console.log(user);
+            })
+            .catch(error => {
+                setErrorMessage(error.message);
+                console.error(error.message);
+            })
+    }
+
+    const handleBlur = event => {
+        SetUserEmail(event.target.value)
+    }
+
+    const handleResetPassword = () => {
+        sendPasswordResetEmail(auth, userEmail)
+        .then(() => {
+            setResetEmail(true);
         })
-        .catch(error =>{
-            setErrorMessage(error);
-            console.error('Error: ', error);
-        })
+        .catch(error => errorMessage(error.message))
     }
 
     return (
         <div>
             {
-                currentUser.uid ? <h2>Hello... <span style={{color: "green"}}>{currentUser.email}</span></h2> : <h2>Please SignIn</h2>
+                currentUser.uid ? <h2>Hello... <span style={{ color: "green" }}>{currentUser.email}</span></h2> : <h2>Please SignIn</h2>
             }
             <form onSubmit={handleLogin}>
                 <label htmlFor="email">Email</label>
-                <input type="email" name='email' required placeholder='Your Email' /><br />
+                <input type="email" name='email' required placeholder='Your Email' onBlur={handleBlur} /><br />
                 <label htmlFor="password">Password</label>
                 <input type="password" name='password' required placeholder='Your Password' /><br />
                 <button type='submit'>Login</button>
             </form>
+            <p>Forget Password? <button onClick={handleResetPassword}>Please reset</button></p>
             <Link to='/'>Don't have an account</Link>
             {
-                errorMessage? <p>Error: {errorMessage}</p> : undefined
+                errorMessage ? <h3 style={{ color: 'red' }}>Error: {errorMessage}</h3> : undefined
+            }
+            {
+                success && <h3 style={{ color: "green" }}>Successfully Loges In</h3>
+            }
+            {
+                resetEmail && <h4>Password reset email send!!!</h4>
             }
         </div>
     );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, parseActionCodeURL } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, parseActionCodeURL, sendEmailVerification } from "firebase/auth";
 import { app } from '../../firebase/firebase.init';
 import { Link } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const auth = getAuth(app);
 
 const Register = () => {
-    const [currentUser, setCurrentUser] = useState({});
+    const [user, setUser] = useState({});
     const [errorMessage, setErrorMessage] = useState('');
 
     const handleCreateUser = event => {
@@ -16,38 +18,64 @@ const Register = () => {
         const password = form.password.value;
         console.log(email, password)
 
+        //validation password with regex
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setErrorMessage('The password must contain at least one Uppercase Character');
+            return;
+        }
+        if (password.length < 6) {
+            setErrorMessage('The password should be at least 6 Characters long');
+            return;
+        }
 
+        //creating user with email&password
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
                 const user = result.user;
-                setCurrentUser(user);
+                setUser(user);
+                form.reset();
+                verifyEmail();
                 console.log(user);
             })
             .catch(error => {
                 console.error('error: ', error)
-                setErrorMessage(error);
+                setErrorMessage(error.message);
             })
-        form.reset();
+
+
+        //email verification
+        const verifyEmail = () => {
+            sendEmailVerification(auth.currentUser)
+                .then(() => {
+                    notify();
+                })
+        }
+
+        //toast
+        const notify = () => {
+            toast('Email Verification send to you email')
+        }
     }
 
     return (
         <div>
             {
-                currentUser.uid ? <h2>Welcome <span style={{color: 'red'}}>{currentUser.email}</span></h2> : <h2>Please Register</h2>
+                user.uid ? <h2>Welcome <span style={{ color: 'green' }}>{user.email}</span></h2> : <h2>Please Register</h2>
             }
             <form onSubmit={handleCreateUser}>
                 <label htmlFor="email">Email</label>
-                <input type="email" placeholder='Your email' name='email' required/><br />
+                <input type="email" placeholder='Your email' name='email' required /><br />
                 <label htmlFor="password">Password</label>
-                <input type="password" placeholder='Your password' name='password' required/><br />
+                <input type="password" placeholder='Your password' name='password' required /><br />
                 <button type='submit'>Register</button>
             </form>
             {
-                errorMessage ? <p><small>errorMessage</small></p>
+                errorMessage ? <p style={{ color: "red" }}>{errorMessage}</p>
                     :
                     undefined
             }
             <Link to='/login'>Already have an account</Link>
+            <ToastContainer />
         </div>
     );
 };
