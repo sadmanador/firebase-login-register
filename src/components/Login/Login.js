de0ad48cf949fb614c05e1151ace4e513ae9ecce
sadmanador <107940313+GaziTasnuvaFarhana@users.noreply.github.com>
2022-12-08 19:06:49 +0600

    login and logup successfully work

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from '../../firebase/firebase.init';
+import { useState } from 'react';
+
+const auth = getAuth(app)
 
 const Login = () => {
+    const [currentUser, setCurrentUser] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const handleLogin = event = {
+    const handleLogin = event => {
         event.preventDefault();
-        
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const user = result.user;
+            setCurrentUser(user);
+        })
+        .catch(error =>{
+            setErrorMessage(error);
+            console.error('Error: ', error);
+        })
     }
 
     return (
         <div>
+            {
+                currentUser.uid ? <h2>Hello... <span style={{color: "green"}}>{currentUser.email}</span></h2> : <h2>Please SignIn</h2>
+            }
             <form onSubmit={handleLogin}>
                 <label htmlFor="email">Email</label>
-                <input type="email" name='email' required placeholder='Your Email.com' /><br />
+                <input type="email" name='email' required placeholder='Your Email' /><br />
                 <label htmlFor="password">Password</label>
                 <input type="password" name='password' required placeholder='Your Password' /><br />
                 <button type='submit'>Login</button>
             </form>
+            <Link to='/'>Don't have an account</Link>
+            {
+                errorMessage? <p>Error: {errorMessage}</p> : undefined
+            }
         </div>
     );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword, getAuth, parseActionCodeURL } from "firebase/auth";
 import { app } from '../../firebase/firebase.init';
+import { Link } from 'react-router-dom';
 
 const auth = getAuth(app);
 
 const Register = () => {
+    const [currentUser, setCurrentUser] = useState({});
     const [errorMessage, setErrorMessage] = useState('');
 
     const handleCreateUser = event => {
@@ -16,22 +18,36 @@ const Register = () => {
 
 
         createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(error => console.error('error: ', error))
+            .then(result => {
+                const user = result.user;
+                setCurrentUser(user);
+                console.log(user);
+            })
+            .catch(error => {
+                console.error('error: ', error)
+                setErrorMessage(error);
+            })
+        form.reset();
     }
 
     return (
         <div>
+            {
+                currentUser.uid ? <h2>Welcome <span style={{color: 'red'}}>{currentUser.email}</span></h2> : <h2>Please Register</h2>
+            }
             <form onSubmit={handleCreateUser}>
                 <label htmlFor="email">Email</label>
-                <input type="email" placeholder='Your email' name='email'/><br />
+                <input type="email" placeholder='Your email' name='email' required/><br />
                 <label htmlFor="password">Password</label>
-                <input type="password" placeholder='Your password' name='password'/><br />
+                <input type="password" placeholder='Your password' name='password' required/><br />
                 <button type='submit'>Register</button>
             </form>
+            {
+                errorMessage ? <p><small>errorMessage</small></p>
+                    :
+                    undefined
+            }
+            <Link to='/login'>Already have an account</Link>
         </div>
     );
 };
