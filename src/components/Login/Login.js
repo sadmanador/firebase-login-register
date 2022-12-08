d1909052f08c911bc5fa952e206c6eec0fb77420
sadmanador <107940313+GaziTasnuvaFarhana@users.noreply.github.com>
2022-12-08 18:29:14 +0600

    Login component is under work

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const Login = () => {
+
+    const handleLogin = event = {
+        event.preventDefault();
+        
+    }
+
     return (
         <div>
-            <h1>this is login page</h1>
+            <form onSubmit={handleLogin}>
+                <label htmlFor="email">Email</label>
+                <input type="email" name='email' required placeholder='Your Email.com' /><br />
+                <label htmlFor="password">Password</label>
+                <input type="password" name='password' required placeholder='Your Password' /><br />
+                <button type='submit'>Login</button>
+            </form>
         </div>
     );
 };
